7ccdb47499197b5bd1467105438d536cb2a2b2de
“Nicola <nicola.z@gmx.com>
2022-09-05 16:55:08 -0400

    v19

--- src/2.Corps/sections/compDesti/Trajets.js
+++ src/2.Corps/sections/compDesti/Trajets.js
@@ -2,22 +2,49 @@ import styled from "styled-components";
 import { useState } from "react";
 
 import trajetsDB from "./donnees/trajets.json";
+import DetailsDuTrajet from "./comp/DetailsDuTrajet";
 
 const Trajets = () => {
+
+    const [trajetChoisi, setTrajetChoisi] = useState();
+    const [trajetSelect, setTrajetSelect] = useState();
+
+    const choisirTrajet = (e) => {
+        console.log(e.target.value);
+        setTrajetChoisi(e.target.value)
+    }
+
+    const changerTrajet = (nouveau) => {
+        console.log(nouveau);
+        setTrajetChoisi(nouveau)
+        setTrajetSelect(nouveau)
+    }
+
     return (
         <Wrapper>
             <Selectionnez>
-                <label>Sélectionnez un trajet :</label>
-                <select>
+                <label htmlFor="selecteurDeTrajet">Sélectionnez un trajet :</label>
+                <select
+                    id="selecteurDeTrajet"
+                    onChange={choisirTrajet}
+                    value={trajetSelect}
+                >
                     {
                         trajetsDB.map((item, index) => {
                             return (
-                                <option key={index}>{item.trajet}</option>
+                                <option key={index} >{item.trajet}</option>
                             )
                         })
                     }
                 </select>
             </Selectionnez>
+            {
+                trajetChoisi !== undefined &&
+                <DetailsDuTrajet
+                    itineraire={trajetChoisi}
+                    changerTrajet={changerTrajet}
+                />
+            }
         </Wrapper>
     )
 }
---
+++ src/2.Corps/sections/compDesti/comp/DetailsDuTrajet.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { ExternalLink } from "react-external-link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import trajetsDB from "../donnees/trajets.json";
+import { faBicycle } from "@fortawesome/free-solid-svg-icons";
+
+const DetailsDuTrajet = ({ itineraire, changerTrajet }) => {
+    console.log(itineraire);
+
+    const details = trajetsDB.find(item => item.trajet === itineraire)
+    console.log(details);
+
+    return (
+        <Wrapper>
+            <h2>Aperçu de {details.trajet}</h2>
+            <div>Image à venir...</div>
+            <div>
+                <p>Distance : {details.distance} km.</p>
+                <p>Véloroutes : </p>
+                <p>Proportion sur piste cyclable : {details.proportion} %.</p>
+                <p>Terrain : {details.terrain}</p>
+            </div>
+            <h2>Notes de l'auteur</h2>
+            <div>
+                <p>Déjà pédalé : </p>
+                <p>
+                    <span>Appréciation personnelle de 1 à 5 :</span>
+                    <span>
+                        {
+                            // details.appreciation.map((item, index) => {
+                            //     return <FontAwesomeIcon icon={faBicycle} />
+                            // })
+                            [...Array(details.appreciation)].map((e, i) => <FontAwesomeIcon key={i} icon={faBicycle} />)
+                        }
+                    </span>
+                </p>
+                <p>Coups de cœur : {details.coups}</p>
+                <p>Défis rencontrés : {details.defis}</p>
+                <p>Autres notes : {details.notes}</p>
+            </div>
+            <h2>Carte</h2>
+            <iframe
+                frameBorder="0"
+                allowFullScreen
+                src={details.carteURL}
+            />
+            <ExternalLink href="https://umap.openstreetmap.fr/fr/map/carte-generale-cyclotouristechezsoi_584684">Voir en plein écran</ExternalLink>
+            <h2>Trajets associés</h2>
+            <div>
+                {
+                    details.ass.map((item, index) => {
+                        return <button onClick={() => changerTrajet(item)} key={index}>{item}</button>
+                    })
+                }
+            </div>
+            <h2>Galerie des photos</h2>
+            <p>Cliquez sur les images ci-dessous pour rêver!</p>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div``
+
+export default DetailsDuTrajet;
