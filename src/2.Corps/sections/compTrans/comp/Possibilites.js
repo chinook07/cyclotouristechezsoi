6e4525e1ab9521c3d3c73445f5ee9f98f5a1fa57
“Nicola <nicola.z@gmx.com>
2022-09-10 14:29:53 -0400

    v34

--- src/2.Corps/sections/compTrans/comp/Possibilites.js
+++ src/2.Corps/sections/compTrans/comp/Possibilites.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import indexVilles from "../donnees/indexVilles.json";
 import trajets from "../donnees/trajets.json"
+import { faTrain, faTrainSubway, faBusAlt, faBus, faFerry } from "@fortawesome/free-solid-svg-icons";
 
 const Possibilites = ({ villeSouhaitee }) => {
+
+    const toutesIcones = [faTrain, faTrainSubway, faBusAlt, faBus, faFerry]
     
     const trajetsLocaux = indexVilles[villeSouhaitee];
     const routes = []
@@ -17,8 +21,11 @@ const Possibilites = ({ villeSouhaitee }) => {
                 villeSouhaitee &&
                 routes.map((item, index) => {
                     return <Itineraire key={index}>
-                        <div>{item.infoA}</div>
-                        <div>{item.infoB}</div>
+                        <div>{index + 1} – <FontAwesomeIcon icon={toutesIcones[item.icone]} /></div>
+                        <div>{item.trajet}</div>
+                        <div>Période d'opération : {item.heures}</div>
+                        <div>{item.velos}</div>
+                        <div>Transporteur : {item.transporteur}</div>
                     </Itineraire>
                 })
             }
