4e35c06724a3bc47a78e76445d6b8a9d4a5d8df2
“Nicola <nicola.z@gmx.com>
2022-09-09 21:33:27 -0400

    v33

--- src/2.Corps/sections/compTrans/Intro.js
+++ src/2.Corps/sections/compTrans/Intro.js
@@ -22,16 +22,6 @@ const Intro = () => {
         <Wrapper>
             <p>Que ce soit pour partir en cyclotourisme dans une région autre que la sienne ou simplement pour effectuer un trajet linéaire – c'est-à-dire pas en boucle, il peut s'avérer nécessaire de mettre son vélo sur un train ou un autocar. Mais comment s'éviter les ennuis, et surtout, comment naviguer à travers la panoplie de règlements qui diffèrent d'un transporteur à l'autre?</p>
             <DeuxFigures photo1={photo1} photo2={photo2} />
-            {/* <div>
-                <figure>
-                    <img src="" alt="Un vélo dans un sac géant et les sacoches aux alentours."></img>
-                    <figcaption>Parfois, un sac géant fait l'affaire pour un voyage en autocar!</figcaption>
-                </figure>
-                <figure>
-                    <img src="" alt="L'auteur qui attache son vélo à sa place à bord d'un train exo."></img>
-                    <figcaption>L'accès aux trains de banlieue se fait à la plupart des gares par l'entremise de quelques marches. Montez vos sacoches lourdes d'abord, ensuite votre vélo.</figcaption>
-                </figure>
-            </div> */}
         </Wrapper>
     )
 }
--- src/2.Corps/sections/compTrans/comp/Resultats.js
+++ src/2.Corps/sections/compTrans/comp/Resultats.js
@@ -6,6 +6,29 @@ const Resultats = ({ recherchee, setVilleSouhaitee }) => {
 
     const toutesVilles = Object.keys(indexVilles);
     const total = toutesVilles.filter(ville => ville.toLowerCase().includes(recherchee.toLowerCase()))
+    if (recherchee.toLowerCase().includes("st-")) {
+        let rechercheeDupl1 = recherchee.replace("st-", "saint-")
+        let autresResultats1 = toutesVilles.filter(ville => ville.toLowerCase().includes(rechercheeDupl1.toLowerCase()));
+        autresResultats1.forEach(item => total.push(item))
+    }
+    if (recherchee.toLowerCase().includes("ste")) {
+        let rechercheeDupl2 = recherchee.replace("ste", "sainte")
+        let autresResultats2 = toutesVilles.filter(ville => ville.toLowerCase().includes(rechercheeDupl2.toLowerCase()));
+        autresResultats2.forEach(item => total.push(item))
+    }
+    // if (recherchee.toLowerCase().includes("e")) {
+    //     console.log('e');
+    //     let autresResultats3, rechercheeDup3;
+    //     rechercheeDup3 = recherchee.replace("e", "é")
+    //     autresResultats3 = toutesVilles.filter(ville => ville.toLowerCase().includes(rechercheeDup3.toLowerCase()));
+    //     autresResultats3.forEach(item => total.push(item))
+    //     rechercheeDup3 = recherchee.replace("e", "è")
+    //     autresResultats3 = toutesVilles.filter(ville => ville.toLowerCase().includes(rechercheeDup3.toLowerCase()));
+    //     autresResultats3.forEach(item => total.push(item))
+    //     rechercheeDup3 = recherchee.replace("e", "ê")
+    //     autresResultats3 = toutesVilles.filter(ville => ville.toLowerCase().includes(rechercheeDup3.toLowerCase()));
+    //     autresResultats3.forEach(item => total.push(item))
+    // }
 
     const choisirVille = (ville) => setVilleSouhaitee(ville);
 
@@ -20,11 +43,31 @@ const Resultats = ({ recherchee, setVilleSouhaitee }) => {
                     })
                 }
             </ul>
-            
         </Wrapper>
     )
 }
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+    ul {
+        align-items: center;
+        display: flex;
+        flex-wrap: wrap;
+        list-style: none;
+        padding: 0;
+        li {
+            background-color: var(--c1);
+            border-radius: 3px;
+            cursor: pointer;
+            margin: 0 10px 10px 0;
+            padding: 5px;
+            &:hover {
+                background-color: var(--c2);
+            }
+            &:active {
+                background-color: var(--c3);
+            }
+        }
+    }
+`
 
 export default Resultats;
