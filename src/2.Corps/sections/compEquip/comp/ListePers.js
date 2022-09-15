11937c7bfe4b24e5657693cf539dcbeb964eded7
“Nicola <nicola.z@gmx.com>
2022-09-15 19:54:23 -0400

    v40

--- src/2.Corps/sections/compEquip/comp/ListePers.js
+++ src/2.Corps/sections/compEquip/comp/ListePers.js
@@ -1,34 +1,72 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { faList, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faList, faTrash, faCircleXmark, faCheck, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const ListePers = ({ PackThis }) => {
     
     const [bagagesPers, setBagagesPers] = useState([]);
+    const [entreePers, setEntreePers] = useState("");
 
-    const ajouterItemPers = (e, itemPers) => {
+    const mettreEntreeAJour = (terme) => setEntreePers(terme);
+
+    const ajouterItemPers = (e) => {
         e.preventDefault();
-        console.log("ajout de ", itemPers);
-        setBagagesPers((prev) => [...prev, itemPers])
+        setBagagesPers((prev) => [...prev, entreePers]);
+        setEntreePers("");
+        // stockage
     }
+
+    const viderListePers = () => setBagagesPers([]);
+
+    const enleverItem = (item) => {
+        const nouvelleListe = bagagesPers;
+        nouvelleListe.splice(bagagesPers.indexOf(item), 1);
+        console.log(nouvelleListe);
+        setBagagesPers(nouvelleListe);
+        // ne fonctionne pas bien
+    }
+
+    // useEffect(() => {
+    //     console.log("effect");
+    // }, [bagagesPers])
 
     return (
         <Wrapper>
             <Controle>
                 <h3><FontAwesomeIcon icon={faList} /> Liste personnalisée</h3>
-                <button>
+                <button onClick={viderListePers}>
                     <FontAwesomeIcon icon={faTrash} />
                 </button>
             </Controle>
-            {
-                bagagesPers.length > 0 &&
-                <div>oui</div>
-            }
-            <form onSubmit={() => ajouterItemPers("chose")}>
-                <input placeholder="Ajoutez quelque chose"></input>
-            </form>
+            <Reste>
+                {
+                    bagagesPers.map((item, index) => {
+                        return (
+                            <ChosePers key={index}>
+                                <button>
+                                    <FontAwesomeIcon icon={faPlus} />
+                                    <span>{item}</span>
+                                </button>
+                                <button onClick={() => enleverItem(item)}>
+                                    <FontAwesomeIcon icon={faCircleXmark} />
+                                </button>
+                                
+                            </ChosePers>
+                        ) 
+                    })
+                }
+                <form onSubmit={ajouterItemPers}>
+                    <input
+                        onChange={(e) => mettreEntreeAJour(e.target.value)}
+                        placeholder="Ajoutez quelque chose"
+                        value={entreePers}
+                    />
+                </form>
+            </Reste>
+                    
+            
             
         </Wrapper>
     )
@@ -42,6 +80,32 @@ const Controle = styled.div`
     > button {
         margin-left: 10px;
         padding: 5px 10px;
+    }
+`
+
+const Reste = styled.div`
+    align-items: center;
+    display: flex;
+    flex-wrap: wrap;
+    input {
+        border-radius: 15px;
+        margin: 5px;
+        padding: 8px 12px;
+    }
+`
+
+const ChosePers = styled.div`
+    border: 2px solid var(--c3);
+    border-radius: 15px;
+    
+    margin: 5px;
+    padding: 8px 12px;
+    button {
+        border: none;
+        cursor: pointer;
+    }
+    svg {
+        margin-left: 5px;
     }
 `
 
--- src/2.Corps/sections/compTrans/TrRegional.js
+++ src/2.Corps/sections/compTrans/TrRegional.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
+import { ExternalLink } from "react-external-link";
 
 const ABCDEF = () => {
     return (
         <TrRegional>
-            <div>Montréal</div>
-            <div>Autres régions</div>
+            <h2>Sortir de la région métropolitaine</h2>
+            <p>Pour sortir de la région métropolitaine, la situation s'est grandement améliorée au fil des années. Parmi les onze secteurs d'exo, le transporteur qui opère les trains de banlieue et les autobus des banlieues de Montréal, <ExternalLink href="https://exo.quebec/fr/planifier-trajet/velo/velo-a-bord">sept en offrent la possibilité</ExternalLink> de transporter son vélo à bord des bus, avec des heures et des dates d'utilisation harmonisées depuis quelques années.</p>
+            <p>Normalement, jusqu'à deux vélos se mettent <ExternalLink href="https://exo.quebec/Media/Default/pdf/section4/velo-bus/Instructions_velobus.pdf">sur le support situé à l'avant</ExternalLink>. Il faut s'assurer de bien placer le bras d'appui le plus près de la fourche possible. Pour aller à Sorel-Tracy ou à Vaudreuil avec les lignes express, il fallait jadis mettre son vélo non démonté en soute, mais ce n'est malheureusement plus possible depuis 2020.</p>
+            <p>Pour ce qui est du train de banlieue, il n'y a plus de restrictions à l'heure de pointe, ni de limites sur le nombre de vélos acceptés par train. Notez par contre que l'accès aux voitures de train est ardu en raison des marches à pic et de l'étroitesse des portes.</p>
+            <h2>Se déplacer dans les régions du Québec</h2>
+            <p>Quelques transporteurs en dehors de Montréal offrent la combinaison vélo-bus ou vélo-train sur une échelle régionale, c'est-à-dire pour des distances de quelques dizaines de kilomètres. Cette catégorie s'avère utile en voyage pour sauter par-dessus une étape particulièrement difficile, par exemple entre Ste-Anne-de-Beaupré et Baie-St-Paul (Charlevoix), ou dans le nord-est de la Gaspésie.</p>
+            <div>Tableau</div>
         </TrRegional>
     )
 }
