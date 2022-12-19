cd2c842c59a4e808c57eca7f15ca5a985eb40fe5
“Nicola <nicola.z@gmx.com>
2022-12-18 21:30:24 -0500

    v128

--- client/src/2.Corps/sections/compComm/comp/TelevPhotos.js
+++ client/src/2.Corps/sections/compComm/comp/TelevPhotos.js
@@ -1,32 +1,32 @@
-// import styled from "styled-components";
-import { useState } from "react";
-import { create, FilePond, registerPlugin } from 'react-filepond';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-import 'filepond/dist/filepond.min.css';
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+// // import styled from "styled-components";
+// import { useState } from "react";
+// import { create, FilePond, registerPlugin } from 'react-filepond';
+// import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+// import 'filepond/dist/filepond.min.css';
+// import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
-registerPlugin(FilePondPluginImagePreview);
+// registerPlugin(FilePondPluginImagePreview);
 
-const TelevPhotos = () => {
+// const TelevPhotos = () => {
 
-    const [photos, setPhotos] = useState([])
+//     const [photos, setPhotos] = useState([])
 
-    const handle = (e) => {
-        console.log(e);
-        setPhotos(photos)
-    }
+//     const handle = (e) => {
+//         console.log(e);
+//         setPhotos(photos)
+//     }
 
-    return (
-        <FilePond
-            allowMultiple={true}
-            files={photos}
-            onupdatefiles={handle}
-            name="photo"
-            labelIdle="Glissez vos fichiers ici ou <span class='filepond--label-action'>parcourir</span>"
-        ></FilePond>
-    )
-}
+//     return (
+//         <FilePond
+//             allowMultiple={true}
+//             files={photos}
+//             onupdatefiles={handle}
+//             name="photo"
+//             labelIdle="Glissez vos fichiers ici ou <span class='filepond--label-action'>parcourir</span>"
+//         ></FilePond>
+//     )
+// }
 
-// const Wrapper = styled.div``
+// // const Wrapper = styled.div``
 
-export default TelevPhotos;
+// export default TelevPhotos;
