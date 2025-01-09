8c4228b505d420b93f1d0295fb749daef4fce4da
“Nicola <nicola.z@gmx.com>
2025-01-09 11:37:58 -0500

    modifs train Charlevoix et exo

--- client/src/2.Corps/sections/compTrans/TrRegional.js
+++ client/src/2.Corps/sections/compTrans/TrRegional.js
@@ -1,32 +1,32 @@
 import { ExternalLink } from "react-external-link";
 
-import DeuxFigures from "../../../CompReutilisables/DeuxFigures";
-import trainCharlevoix1 from "./images/train-charlevoix-1.webp"
-import trainCharlevoix2 from "./images/train-charlevoix-2.webp"
+// import DeuxFigures from "../../../CompReutilisables/DeuxFigures";
+// import trainCharlevoix1 from "./images/train-charlevoix-1.webp"
+// import trainCharlevoix2 from "./images/train-charlevoix-2.webp"
 
 import TableauRegions from "./comp/TableauRegions";
 import navettes from "./donnees/navettes.json";
 
 const TrRegional = () => {
 
-    const photo1 = {
-        "src": trainCharlevoix1,
-        "alt": "Un petit train devant une énorme basilique",
-        "cap": "Le train de Québec vers la région de Charlevoix"
-    }
+    // const photo1 = {
+    //     "src": trainCharlevoix1,
+    //     "alt": "Un petit train devant une énorme basilique",
+    //     "cap": "Le train de Québec vers la région de Charlevoix"
+    // }
 
-    const photo2 = {
-        "src": trainCharlevoix2,
-        "alt": "Fenêtre d'un train, fleuve, plage et montagne à l'extérieur",
-        "cap": "Une vue splendide tout le long du chemin"
-    }
+    // const photo2 = {
+    //     "src": trainCharlevoix2,
+    //     "alt": "Fenêtre d'un train, fleuve, plage et montagne à l'extérieur",
+    //     "cap": "Une vue splendide tout le long du chemin"
+    // }
 
     return (
         <div>
             <section>
                 <h2>Sortir de la région métropolitaine</h2>
                 <p>Pour sortir de la région métropolitaine, la situation s'est grandement améliorée au fil des années. Parmi les onze secteurs d'exo, le transporteur qui opère les trains et les autobus dans la banlieue de Montréal, <ExternalLink href="https://exo.quebec/fr/planifier-trajet/velo/velo-a-bord">sept en offrent la possibilité</ExternalLink> de transporter son vélo à bord des bus, avec des heures et des dates d'utilisation harmonisées depuis quelques années. Jusqu'à deux vélos peuvent être placés <ExternalLink href="https://exo.quebec/Media/Default/pdf/section4/velo-bus/Instructions_velobus.pdf">sur le support situé à l'avant (PDF)</ExternalLink>. Il faut s'assurer de bien placer le bras d'appui le plus près de la fourche possible.</p>
-                <p>Pour ce qui est du train de banlieue, il n'y a pas de restrictions à l'heure de pointe, ni de limites sur le nombre de vélos acceptés par train. Notez par contre que l'accès aux voitures de train est ardu en raison des marches à pic et de l'étroitesse des portes.</p>
+                <p>Pour ce qui est du train de banlieue, il n'y a pas de restrictions à l'heure de pointe, ni de limites sur le nombre de vélos acceptés par train. Notez par contre que l'accès aux voitures de train est ardu en raison des marches à pic et de l'étroitesse des portes. Par contre, de nouvelles voitures plus accessibles circulent sur certains départs de la ligne Saint-Jérôme.</p>
                 <p><ExternalLink href="https://www.stm.info/fr/velo/bienvenue-aux-velos">Le métro de Montréal</ExternalLink>, ainsi que <ExternalLink href="https://rem.info/fr/se-deplacer/faq-sur-les-deplacements/est-ce-que-je-peux-transporter-mon-velo-dans-le-rem">le REM</ExternalLink>, avec un service bien plus fréquent que le train de banlieue, offrent la possibilité de transporter son vélo en semaine avant 7 h, entre 9 h 30 et 15 h 30, ainsi qu'en soirée après 18 h. Les vélos sont permis en tout temps le samedi et le dimanche. Certaines exceptions peuvent s'appliquer <ExternalLink href="https://www.stm.info/fr/velo/calendrier-des-restrictions-lors-devenements">lors d'évènements générant de l'achalandage</ExternalLink>. Durant la période estivale (fin mai à fin aout), les vélos sont autorisés en tout temps!</p>
             </section>
             <section>
