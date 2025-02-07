8c9c408726b7febed24a430dacfa7d340b738272
“Nicola <nicola.z@gmx.com>
2025-02-07 16:06:48 -0500

    conférence et blogue

--- client/src/2.Corps/accueil/comp/Conference.js
+++ client/src/2.Corps/accueil/comp/Conference.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ExternalLink } from "react-external-link";
 
 const Conference = () => {
@@ -14,6 +13,12 @@ const Conference = () => {
             </ul>
             <p>L’idée née durant la pandémie était de sillonner de fond en comble une région entière à proximité de la maison. La première boucle d’une semaine m’a fait tomber amoureux de la Montérégie, ses rangs bucoliques, ses cours d’eau et ses belles pistes cyclables. Le deuxième était 2 fois plus long et 3 fois plus difficile. Elle m’amena à mieux découvrir les Cantons-de-l’Est, ses pénibles côtes, dont le mont Mégantic, ainsi que ses charmants villages et ses centaines d’attraits. En 2024, les astres s’étaient enfin alignés pour une virée au Vermont. J’ai donc prévu une boucle de 10 jours pour rouler sur la toute nouvelle Lamoille Valley Rail Trail dans le nord de l’état.</p>
             <p>Pas besoin d’aller très loin pour se dépayser en cyclotourisme. Découvrez avec moi les plus belles routes et pistes cyclables à proximité de Montréal. On abordera aussi la planification d’un itinéraire, l’hébergement, ainsi que les catastrophes naturelles!</p>
+            <h3>Suivez-moi pendant la soirée :</h3>
+            <ol>
+                <li>La Montérégie : <ExternalLink href="https://cycle.travel/map/journey/620147">Carte</ExternalLink> <ExternalLink href="https://www.crazyguyonabike.com/doc/tourmonteregie">Récit</ExternalLink></li>
+                <li>Les Cantons-de-l'Est : <ExternalLink href="https://cycle.travel/map/journey/620148">Carte</ExternalLink> <ExternalLink href="https://www.crazyguyonabike.com/doc/tourcantons">Récit</ExternalLink></li>
+                <li>Le nord du Vermont : <ExternalLink href="https://cycle.travel/map/journey/671819">Carte</ExternalLink> <ExternalLink href="https://www.crazyguyonabike.com/doc/nordvermont">Récit</ExternalLink></li>
+            </ol>
         </Wrapper>
     )
 }
