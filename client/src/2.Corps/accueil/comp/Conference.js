51856df5a3074fec41046b388378c452b3769933
“Nicola <nicola.z@gmx.com>
2026-01-14 11:18:58 -0500

    nouvelle conférence

--- client/src/2.Corps/accueil/comp/Conference.js
+++ client/src/2.Corps/accueil/comp/Conference.js
@@ -4,20 +4,19 @@ import { ExternalLink } from "react-external-link";
 const Conference = () => {
     return (
         <Wrapper>
-            <h2>Conférence récente</h2>
+            <h2>Conférence prochaine</h2>
             <ul>
-                <li>Cyclotourisme de proximité : trois régions à découvrir tout près</li>
-                <li>Date : <time dateTime="2025-02-10 19:00">10 février 2025 à 19 h sur le web</time></li>
-                <li>Inscription avec <ExternalLink href="https://www.velo.qc.ca/evenement/conference-et-salon/conference-velo-maison-des-cyclistes/conferences-velo-mag/nicola-zoghb-cyclotourisme-proximite/">Vélo Québec</ExternalLink></li>
+                <li>Célébrer les 30 ans de la Route verte, en dyade père-fils et en solo</li>
+                <li>Date : <time dateTime="2026-03-02 19:00">2 mars 2026 à 19 h sur le web</time></li>
+                <li>Inscription avec <ExternalLink href="https://www.velo.qc.ca/evenement/conference-et-salon/conference-velo-maison-des-cyclistes/conferences-velo-mag/celebrer-la-30e-annee-de-la-route-verte-en-dyade-pere-fils-et-en-solo/">Vélo Québec</ExternalLink></li>
                 <li>Prix : 8,70 $ + taxes (gratuit pour les membres)</li>
             </ul>
-            <p>L’idée née durant la pandémie était de sillonner de fond en comble une région entière à proximité de la maison. La première boucle d’une semaine m’a fait tomber amoureux de la Montérégie, ses rangs bucoliques, ses cours d’eau et ses belles pistes cyclables. Le deuxième était 2 fois plus long et 3 fois plus difficile. Elle m’amena à mieux découvrir les Cantons-de-l’Est, ses pénibles côtes, dont le mont Mégantic, ainsi que ses charmants villages et ses centaines d’attraits. En 2024, les astres s’étaient enfin alignés pour une virée au Vermont. J’ai donc prévu une boucle de 10 jours pour rouler sur la toute nouvelle Lamoille Valley Rail Trail dans le nord de l’état.</p>
-            <p>Pas besoin d’aller très loin pour se dépayser en cyclotourisme. Découvrez avec moi les plus belles routes et pistes cyclables à proximité de Montréal. On abordera aussi la planification d’un itinéraire, l’hébergement, ainsi que les catastrophes naturelles!</p>
+            <p>En 2025, j'ai trouvé l’occasion parfaite pour initier mon fils de deux ans au cyclotourisme. Après quelques hésitations, j'ai tracé un itinéraire adapté, de Montréal jusqu’à Sherbrooke, avec comme point d’arrivée symbolique l’Université Bishop’s. Sur cinq jours, mon fils et moi avons pédalé à notre rythme, empruntant tantôt la Route verte, tantôt des pistes cyclables ou des routes plus directes.</p>
+            <p>Le mois suivant, je me suis offert un voyage en solo. J'ai enfourché mon vélo pour parcourir le plus emblématique parc linéaire du Québec : le P’tit train du Nord. De là, j'ai poursuivi ma route sur la Véloroute des Draveurs et la nouvelle Voie verte Chelsea. Les gares patrimoniales, les rivières, les paysages variés et les rencontres marquantes jalonnent ce périple, qui s’inscrit comme un hommage aux 30 ans de la Route verte, l’un des plus beaux réseaux cyclables des Amériques.</p>
             <h3>Suivez-moi pendant la soirée :</h3>
             <ol>
-                <li>La Montérégie : <ExternalLink href="https://cycle.travel/map/journey/620147">Carte</ExternalLink> <ExternalLink href="https://www.crazyguyonabike.com/doc/tourmonteregie">Récit</ExternalLink></li>
-                <li>Les Cantons-de-l'Est : <ExternalLink href="https://cycle.travel/map/journey/620148">Carte</ExternalLink> <ExternalLink href="https://www.crazyguyonabike.com/doc/tourcantons">Récit</ExternalLink></li>
-                <li>Le nord du Vermont : <ExternalLink href="https://cycle.travel/map/journey/671819">Carte</ExternalLink> <ExternalLink href="https://www.crazyguyonabike.com/doc/nordvermont">Récit</ExternalLink></li>
+                <li>Papa et fils vers Bishop's : <ExternalLink href="https://cycle.travel/map/journey/909918">Carte</ExternalLink> <ExternalLink href="https://www.crazyguyonabike.com/doc/papaetfils">Récit</ExternalLink></li>
+                <li>Finir le triangle de l'ouest : <ExternalLink href="https://cycle.travel/map/journey/909920">Carte</ExternalLink> <ExternalLink href="https://www.crazyguyonabike.com/doc/boucleouest">Récit</ExternalLink></li>
             </ol>
         </Wrapper>
     )
