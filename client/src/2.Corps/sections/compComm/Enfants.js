7a346e0638d5c99c1e966046ae0d6a20aff2ad37
“Nicola <nicola.z@gmx.com>
2026-02-20 11:42:26 -0500

    infos enfants

--- client/src/2.Corps/sections/compComm/Enfants.js
+++ client/src/2.Corps/sections/compComm/Enfants.js
@@ -8,6 +8,7 @@ const Enfants = () => {
     return (
         <Wrapper>
             <h2>Voyager avec des enfants</h2>
+            <p>Attention! Les informations sur cette page sont encore en rédaction.</p>
             <p>On dit que la naissance d'un enfant bouscule les habitudes du quotidien. C'est vrai, mais ce n'empêche pas la pratique du cyclotourisme, moyennant quelques changements pour que tous puissent s'amuser.</p>
             <h3>Les différents systèmes de transport</h3>
             <p>Il existe plusieurs façons de transporter un enfant qui ne nécessitent pas l'achat d'un vélo-cargo onéreux.</p>
@@ -16,7 +17,7 @@ const Enfants = () => {
             <p>Tel qu'on constate dans le tableau ci-dessus, il n'y a pas d'approche universelle. Chaque système a ses avantages et ses inconvénients. Parfois, il peut être judicieux de combiner deux systèmes pour le bonheur des parents et de l'enfant.</p>
             <MeilleursChoix />
             <h3>Autres conseils</h3>
-            <p>Pour mieux réussir votre voyage... (à terminer)</p>
+            <p>En cours de rédaction</p>
         </Wrapper>
     )
 }
