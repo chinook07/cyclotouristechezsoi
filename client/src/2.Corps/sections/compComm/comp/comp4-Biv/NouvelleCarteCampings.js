a3407a2f252485d48bea39abc8c6479d9ae4fce3
“Nicola <nicola.z@gmx.com>
2026-02-26 21:33:45 -0500

    carte v3 et avis Dependabot

--- client/src/2.Corps/sections/compComm/comp/comp4-Biv/NouvelleCarteCampings.js
+++ client/src/2.Corps/sections/compComm/comp/comp4-Biv/NouvelleCarteCampings.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NouvelleCarteCampings = () => {
     return (
         <Wrapper>
-            <iframe allowFullScreen allow="geolocation" src="//umap.openstreetmap.fr/fr/map/dormir-gratuitement-en-velo_1365588?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=caption&captionBar=false&captionMenus=true&locateControl=true&captionControl=true" title="carte des campings gratuits"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/dormir-gratuitement-en-velo_1365588?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=caption&captionBar=false&captionMenus=true&locateControl=true&captionControl=true">Voir en plein écran</a></p>
+            <iframe allowFullScreen allow="geolocation" src="//umap.openstreetmap.fr/fr/map/dormir-gratuitement-en-velo_1365588?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=caption&captionBar=false&captionMenus=true&locateControl=true&captionControl=true" title="carte des campings gratuits"></iframe><p><a href="//umap.openstreetmap.fr/fr/map/dormir-gratuitement-en-velo_1365588?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=caption&captionBar=false&captionMenus=true&locateControl=true&captionControl=true">Voir en plein écran</a></p>
         </Wrapper>
     )
 }
